6992113609d42bf55b82517a13018b5d83e5f596
Vikram Sharma <75490688+papafffranku@users.noreply.github.com>
2021-07-23 15:33:45 +0530

    auth

--- lib/abc.dart
+++ lib/abc.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:lessgoo/firesign/GoogleSignInProvider.dart';
 import 'package:provider/provider.dart';
 
@@ -12,38 +14,48 @@ class abc extends StatefulWidget {
 }
 
 class _abcState extends State<abc> {
+  final googleSignIn = GoogleSignIn();
+
+  GoogleSignInAccount? _user;
+
+  GoogleSignInAccount get user => _user!;
+
   @override
-  Widget build(BuildContext context) => ChangeNotifierProvider(
-      create: (context) => GoogleSignInProvider(),
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.white12,
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SignInButton(
-                    Buttons.Google,
-                    onPressed: (){
-                      final provider =
-                        Provider.of<GoogleSignInProvider>(context,listen: false);
-                      provider.googleLogin();
-
-                    }),
-                SignInButton(
-                    Buttons.AppleDark,
-                    onPressed: (){
-
-                    }),
-                CupertinoButton(child: Text('Sign up with Username and password'), onPressed: (){})
-              ],
-            ),
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white12,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SignInButton(Buttons.Google, onPressed: () {
+                goochapati();
+              }),
+              SignInButton(Buttons.AppleDark, onPressed: () {}),
+              CupertinoButton(
+                  child: Text('Sign up with Username and password'),
+                  onPressed: () {})
+            ],
           ),
         ),
       ),
-  );
+    );
+  }
 
-  Future goochapati()async{
+  Future goochapati() async {
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser == null) return;
+    _user = googleUser;
+
+    final googleAuth = await googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    await FirebaseAuth.instance.signInWithCredential(credential);
 
   }
 }
